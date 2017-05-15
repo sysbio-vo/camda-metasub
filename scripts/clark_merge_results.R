e2915f6273571e9e127540ec192a88752478d27b
aln <alnfgit@gmail.com>
2017-05-15 17:54:46 +0300

    convert data to metagenomeSeq required format

--- scripts/clark_merge_results.R
+++ scripts/clark_merge_results.R
@@ -1,4 +1,6 @@
 library(plyr)
+library(metagenomeSeq)
+library(stringr)
 
 city = "boston"
 meta.data <- read.table(paste("../metadata/", city, "_metadata.tsv", sep=""), header = TRUE,
@@ -28,7 +30,20 @@ for (i in 1:length(tables)) {
   colnames(final)[(i*3+1):(i*3+3)] = c(paste(c("Count", "All", "Classified"), ".", meta.data$ID[i], sep=""))
 }
 
+count <- final[,grepl("Count", names(final))]
+colnames(count) <- meta.data$ID
+count[is.na(count)] <- 0
+rownames(count) <- final$TaxID
 
+taxa <- str_split_fixed(final$Lineage, ";", 6)
+taxa <- as.data.frame(taxa)
+taxa <- cbind(final$Lineage, taxa)
+taxa <- cbind(final$TaxID, taxa)
+taxa <- cbind(taxa, final$Name)
+taxa <- cbind(taxa, final$Name)
+colnames(taxa) <- colnames(read.delim(file.path(dataDirectory, "CHK_otus.taxonomy.csv"),
+                             stringsAsFactors = FALSE))
 
-
+taxa$strain <- NA
+taxa <- apply(taxa, 2, function(x) gsub("^$|^ $", NA, x))
 
